00cdd64eab586b6abdf280616eac691fc691c99b
Pedroo-Nietoo <pedronieto.2005@gmail.com>
2023-04-05 19:56:16 -0300

    Add CRUD completo com Node

--- Lógica de Programação/Back-End/CRUD/index.js
+++ Lógica de Programação/Back-End/CRUD/index.js
@@ -16,22 +16,31 @@ server.get('/cursos', (req, res) => {
 
 //Listagem de um curso - GET do CRUD
 server.get('/cursos/:index', (req, res) => {
-    const {index} = req.params; //query = dentro do objeto, params = referenciar algo
+    const { index } = req.params; //query = dentro do objeto, params = referenciar algo
     return res.json(cursos[index]); //Retornando um JSON
     //console.log("Acessou a rota!")
 })
 
 //Criar um curso - POST do CRUD
 server.post('/cursos', (req, res) => {
-    const {name} = req.body; //body = corpo da requisição, tratada para JSON e enviada
+    const { name } = req.body; //body = corpo da requisição, tratada para JSON e enviada
     cursos.push(name);
     return res.json(cursos)
 })
 
+//Edita um curso - PUT do CRUD
+server.put('/cursos/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    cursos[index] = name;
+    return res.json(cursos);
+});
+
+//Deleta um curso - DELETE do CRUD
 server.delete('/cursos/:index', (req, res) => {
-    const {index} = req.params;
-    cursos.split(cursos[index]);
-    return res.json(cursos)
-})
+    const { index } = req.params;
+    cursos.splice(index, 1);
+    return res.json(cursos);
+});
 
 server.listen(3000); //Ouve a porta do servidor
